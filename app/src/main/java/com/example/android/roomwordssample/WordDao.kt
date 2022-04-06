4065491453ab876c3a4dfcc685aaa60ebdffe434
organizer <zetiafifah11@gmail.com>
2022-04-06 19:23:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/roomwordssample/WordDao.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.roomwordssample
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * The Room Magic is in this file, where you map a method call to an SQL query.
+ *
+ * When you are using complex data types, such as Date, you have to also supply type converters.
+ * To keep this example basic, no types that require type converters are used.
+ * See the documentation at
+ * https://developer.android.com/topic/libraries/architecture/room.html#type-converters
+ */
+
+//mengimplementasikan dao
+@Dao
+interface WordDao {
+
+    // The flow always holds/caches latest version of data. Notifies its observers when the
+    // data has changed.
+    //untuk mendapatkan semua kata dan menghasilkan
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): Flow<List<Word>>
+
+    //Mendeklarasikan fungsi penangguhan untuk menyisipkan satu kata.
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    //Mendeklarasikan fungsi penangguhan untuk menghapus semua kata
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
